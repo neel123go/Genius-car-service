84e2988beaac368f6dad59ba54cf71ab683b24bd
neel123go <ayounpaul1029@gmail.com>
2022-04-14 14:13:52 +0600

    firebase facebook authentication added

--- src/Pages/Login/SocialLogin/SocialLogin.js
+++ src/Pages/Login/SocialLogin/SocialLogin.js
@@ -2,13 +2,16 @@ import { React, useEffect } from 'react';
 import FacebookImg from '../../../images/social/facebook.png';
 import GoogleImg from '../../../images/social/google.png';
 import GithubImg from '../../../images/social/github.png';
+import TwitterImg from '../../../images/social/twitter.png';
 import auth from '../../../Firebase.init';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Spinner } from 'react-bootstrap';
 
 const SocialLogin = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, googleUser, googleLoading, googleError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookError] = useSignInWithFacebook(auth);
+
     const location = useLocation();
     const nevigate = useNavigate();
     let from = location.state?.from?.pathname || "/";
@@ -16,18 +19,18 @@ const SocialLogin = () => {
     let errorMsg;
 
     useEffect(() => {
-        if (user) {
+        if (googleUser || facebookUser) {
             nevigate(from, { replace: true });
         }
-    }, [user]);
+    }, [googleUser, facebookUser]);
 
-    if (error) {
+    if (googleError || facebookError) {
         errorMsg = <div className='text-center text-danger'>
-            <p>{error.message}</p>
+            <p>{googleError?.message} {facebookError?.message}</p>
         </div>
     }
 
-    if (loading) {
+    if (googleLoading || facebookLoading) {
         loadingSpinner = <div className='text-center'><Spinner animation="border" variant="primary" /></div>
     }
 
@@ -41,9 +44,10 @@ const SocialLogin = () => {
             {errorMsg}
             {loadingSpinner}
             <div className='text-center mt-3'>
-                <button onClick={() => signInWithGoogle()} className='btn btn-light border border-secondary me-2'><img style={{ width: '23px', height: '24px', marginRight: '5px' }} src={GoogleImg} alt="" /> Continue With Google</button>
-                <button className='btn btn-light border border-secondary me-2'><img style={{ width: '23px', height: '24px', marginRight: '5px' }} src={FacebookImg} alt="" /> Continue With Facebook</button>
-                <button className='btn btn-light border border-secondary'><img style={{ width: '23px', height: '24px', marginRight: '5px' }} src={GithubImg} alt="" /> Continue With Github</button>
+                <button onClick={() => signInWithGoogle()} className='btn btn-light border border-secondary me-3'><img style={{ width: '23px', height: '24px', marginRight: '5px' }} src={GoogleImg} alt="" /> Google</button>
+                <button onClick={() => signInWithFacebook()} className='btn btn-light border border-secondary me-3'><img style={{ width: '23px', height: '24px', marginRight: '5px' }} src={FacebookImg} alt="" /> Facebook</button>
+                <button className='btn btn-light border border-secondary me-3'><img style={{ width: '23px', height: '24px', marginRight: '5px' }} src={GithubImg} alt="" /> Github</button>
+                <button className='btn btn-light border border-secondary'><img style={{ width: '25px', height: '21px', marginRight: '5px' }} src={TwitterImg} alt="" /> Twitter</button>
             </div>
         </div>
     );
